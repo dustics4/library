07f9678774f451203ab0e3acbdde232a88c1ce18
Konrad <konrad.babowicz@gmail.com>
2023-10-16 22:55:14 +0100

    Dialog box shows up Input Doesn't display Unable to close Dialog

--- code.js
+++ code.js
@@ -12,3 +12,32 @@ const theHobbit = new Book('The Hobit by J.R.R.', 'Tolkien' ,'295 pages' , 'not 
 console.log(theHobbit.info());
 
 
+//SHOW THE DIALOG BOX
+
+const showButton = document.getElementById("createBook")
+const bookDialog = document.getElementById("bookDialog")
+const buttonSubmit = document.getElementById("buttonSubmit")
+const buttonClose = document.getElementById("buttonClose")
+ 
+
+const output = document.querySelector("output")
+
+showButton.addEventListener("click", () =>{
+    bookDialog.showModal();
+});
+
+bookDialog.addEventListener("close", (e) => {
+    output.value =
+      bookDialog.returnValue === "default"
+        ? "No return value."
+        : `ReturnValue: ${bookDialog.returnValue}.`; // Have to check for "default" rather than empty string
+});
+
+buttonSubmit.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    bookDialog.close(console.log("sent")); // Have to send the select box value here.
+});
+
+buttonClose.addEventListener("click", () => {
+    bookDialog.closest("nothingChosen");
+})
