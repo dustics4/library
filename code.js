2c25548f7387f70ae3c9a8b893adac4a759c226b
Konrad <konrad.babowicz@gmail.com>
2023-10-18 08:04:37 +0100

    can close the dialog box now

--- code.js
+++ code.js
@@ -18,20 +18,12 @@ const showButton = document.getElementById("createBook")
 const bookDialog = document.getElementById("bookDialog")
 const buttonSubmit = document.getElementById("buttonSubmit")
 const buttonClose = document.getElementById("buttonClose")
- 
-
 const output = document.querySelector("output")
 
 showButton.addEventListener("click", () =>{
     bookDialog.showModal();
 });
 
-bookDialog.addEventListener("close", (e) => {
-    output.value =
-      bookDialog.returnValue === "default"
-        ? "No return value."
-        : `ReturnValue: ${bookDialog.returnValue}.`; // Have to check for "default" rather than empty string
-});
 
 buttonSubmit.addEventListener("click", (event) => {
     event.preventDefault(); // We don't want to submit this fake form
@@ -39,5 +31,5 @@ buttonSubmit.addEventListener("click", (event) => {
 });
 
 buttonClose.addEventListener("click", () => {
-    bookDialog.closest("nothingChosen");
+    bookDialog.close();
 })
