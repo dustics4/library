5a949b2a26209042cfb315f843052433c1622365
Konrad <konrad.babowicz@gmail.com>
2023-10-21 12:43:34 +0100

    submit button now adds input into the array

--- code.js
+++ code.js
@@ -3,20 +3,23 @@ const showButton = document.getElementById("createBook")
 const bookDialog = document.getElementById("bookDialog")
 const buttonSubmit = document.getElementById("buttonSubmit")
 const buttonClose = document.getElementById("buttonClose")
-const output = document.querySelector("output")
+const bookSection = document.querySelector(".main")
 
 //TAKE INPUT
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
 
 showButton.addEventListener("click", () =>{
-    bookDialog.showModal();
+    bookDialog.showModal(); 
 });
 
-buttonClose.addEventListener("click", () => {
+buttonClose.addEventListener("click", (e) => {
+    e.preventDefault();
     bookDialog.close();
 })
+
+
 
 //Array to hold the content of the book
 //how to use push in array
@@ -28,24 +31,37 @@ const myLibrary = [
     }
 ];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
   // the constructor...
   // create the constructor
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+
 }
 
 function addBookToLibrary() {
+    let newBook = new Book(title,author,pages);
+    myLibrary.push(newBook);
 
+    myLibrary.forEach((myLibrary) => {
+        let title = myLibrary.title;
+        console.log(title);
+    })
   // do stuff here
-//user form query selector to take input
-// create variables to put it into here
-// e.g let title = $titleINput.value;
-// When you get to read, create a function getReadValue().checked
-  return new Book(title,author,pages,read);
-  
+    //user form query selector to take input
+    // create variables to put it into here
+    // When you get to read, create a function getReadValue().checked
+}
+
+function printBook(){
+
+}
+
+function clearInput(){
+    title.value = '';
+    author.value = '';
+    pages.value = '';
 }
 
 function createBook(){
@@ -60,8 +76,13 @@ function getReadValue() {
 }
 
 //Create a function to show the input on the page
+
 // When clicking the submit button, it needs to run all the functions
-buttonSubmit.addEventListener("click", (event) => {
-    event.preventDefault(); // We don't want to submit this fake form
-    bookDialog.close(console.log("sent")); // Have to send the select box value here.
-});
+buttonSubmit.addEventListener("click" , (e) => {
+    e.preventDefault();
+    
+    addBookToLibrary();
+    
+    bookDialog.close();
+
+})
